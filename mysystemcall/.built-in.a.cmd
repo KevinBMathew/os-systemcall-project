a588c60358b0ddd899a4a15d4b20d422d2399275
Kevin Mathew <kevin.mathew@outlook.in>
2026-03-31 17:57:03 +0530

    removed useless compilation files

--- mysystemcall/.built-in.a.cmd
+++
@@ -1,1 +0,0 @@
-cmd_mysystemcall/built-in.a := rm -f mysystemcall/built-in.a; echo pubsub.o | sed -E 's:([^ ]+):mysystemcall/\1:g' | xargs ar cDPrST mysystemcall/built-in.a
--- mysystemcall/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_mysystemcall/modules.order := {  :; } | awk '!x[$$0]++' - > mysystemcall/modules.order
